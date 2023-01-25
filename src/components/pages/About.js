26bc5bb6a913e62bc54be44eaa5d3548255a757f
Hugo <hugooloya@gmail.com>
2023-01-25 15:04:53 -0700

    First commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/About.css";
+
+export default function About() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="col">About</h1>
+        <p>
+          Hi, I'm Hugo! Full stack MERN Developer from Denver, Colorado. I have
+          been very passionate about computers since I was 10 years old, exactly
+          when I had to leave my parents without phone line, to go online and
+          print texts on how HTML tags work and bash commands tutorials. Yes,
+          I'm that old. After many years of school and math, I thought computers
+          might not be my strongest skill and pursued filmmaking and
+          photography. Well, turns out I discovered my potential to combine
+          creativity with logical thinking, front-end people call it. I
+          completed my Full Stack MERN Development certificate at the University
+          of Denver Coding Boot Camp, where I acquired my skills. Paying
+          attention to details has been one of my strongest guidelines when it
+          comes to my work, since I believe it makes a huge difference.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Resume() {
+  return <div>HERE GOES MY RESUME</div>;
+}
