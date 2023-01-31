58d3f04610cc4a51095721c10ff26c686dbfa4fb
Hugo <hugooloya@gmail.com>
2023-01-30 17:25:55 -0700

    Routes deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import About from "./components/About";
 import Porfolio from "./components/Portfolio/Portfolio";
@@ -12,16 +12,12 @@ export default function App() {
   return (
     <>
       <Header />
-      <Switch>
-        <Route exact path="/" />
-        <About />
-        <Route path="/Contact" />
-        <Contact />
-        <Route path="/Portfolio" />
-        <Porfolio projects={projects} />
-        <Route path="/Resume" />
-        <Resume />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Portfolio" element={<Porfolio projects={projects} />} />
+        <Route path="/Resume" element={<Resume />} />
+      </Routes>
       <Footer />
     </>
   );
